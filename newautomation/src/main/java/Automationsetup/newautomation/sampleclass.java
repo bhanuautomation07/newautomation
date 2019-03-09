88c9892b66d61b9bf32f8fe785c9003c0f3f2d93
bhanuautomation07 <007.bps.007@gmail.com>
2019-03-09 21:39:15 +0530

    new commit

---
+++ newautomation/src/main/java/Automationsetup/newautomation/sampleclass.java
@@ -0,0 +1,16 @@
+package Automationsetup.newautomation;
+
+public class sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void test() {
+		
+		
+		System.out.println("bhanu new project");
+	}
+
+}
---
+++ newautomation/src/test/java/Automationsetup/newautomation/testclass.java
@@ -0,0 +1,12 @@
+package Automationsetup.newautomation;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+sampleclass ob= new sampleclass();
+
+ob.test();
+	}
+
+}
